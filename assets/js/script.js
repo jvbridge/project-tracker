59fca7f15f7d03dfe1fc1f4165ea5db8083d8332
Jonathan Bridge <jvbridge@gmail.com>
2022-04-07 21:50:02 -0700

    time updates now

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,20 @@
+/*******************************************************************************
+ * Initialization
+ ******************************************************************************/
+// Make the time correct on page load
+updateTime();
+
+// Make it automatically update every second
 setInterval(() => {
-    $("#time").text(moment().format("HH:mm:ss"));
+    updateTime();
 },1000)
+
+
+/*******************************************************************************
+ * functions
+ ******************************************************************************/
+
+function updateTime(){
+    $("#time").text(moment().format("h:mm:ss a"));
+    $("#date").text(moment().format("MMM Do, YYYY"));
+}
